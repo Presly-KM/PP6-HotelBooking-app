d3d4c9ded65d37f69884424802232a35fb31dce0
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-08-07 12:21:01 +0200

    Add Layout component to homepage route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,14 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom";
+import Layout from "./layouts/Layout";
 
+// Le fichier App.tsx est le point d'entrée de l'application React. Il configure les routes de l'application en utilisant React Router.
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<span className="text-4xl">Home Page</span>} />
+        <Route path="/" element={<Layout></Layout>} />               {/* Ici on définit la route principale. Ainsi, quand l'utilisateur accède à la racine de l'application, il sera redirigé vers le composant Layout. On a ajouté le composant Layout à notre page d'acceuil */}
         <Route path="/search" element={<>Search Page</>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
