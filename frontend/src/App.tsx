db70d62cf898a477b83ed0df6711e395bdf68e58
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-08-07 19:26:39 +0200

    Add "homepage" and "Search Page" children in Layout component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,8 +11,17 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout></Layout>} />               {/* Ici on définit la route principale. Ainsi, quand l'utilisateur accède à la racine de l'application, il sera redirigé vers le composant Layout. On a ajouté le composant Layout à notre page d'acceuil */}
-        <Route path="/search" element={<>Search Page</>} />
+        <Route path="/" element={         // Ici on définit la route principale. Ainsi, quand l'utilisateur accède à la racine de l'application, il sera redirigé vers le composant Layout. On a ajouté le composant Layout à notre page d'acceuil 
+          <Layout>
+            <p>Home Page</p>              {/* Ici on utilise le composant Layout pour encapsuler la page d'accueil. le <p> représente le children de notre Layout définie dans App.tsx */}
+          </Layout>
+        } 
+        />                                
+        <Route path="/search" 
+        element={
+         <Layout>
+            <p>Search Page</p>            {/* Ici on utilise le composant Layout pour encapsuler la page de recherche. le <p> représente le children de notre Layout définie dans App.tsx */}
+          </Layout>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
