d305fc5cf1c5aefdb84073c506d55db4428636dd
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-08-08 12:25:20 +0200

    import and implement authRoutes ('/api/auth')

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";  // Importation de mongoose pour la connexion à MongoDB
 import userRoutes from "./routes/users"; // Importation des routes des utilisateurs
+import authRoutes from "./routes/auth"; // Importation des routes d'authentification
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)  // Connexion à MongoDB en utilisant la chaîne de connexion stockée dans le fichier .env. On utilise "as string" pour indiquer à TypeScript que cette variable est une chaîne de caractères. Ainsi, on évite les erreurs de type si la variable n'est pas définie.
 
@@ -11,6 +12,7 @@ app.use(express.json()); // Ici on utilise express.json() pour parser le JSON da
 app.use(express.urlencoded({ extended: true })); // Pour parser les données URL-encoded
 app.use(cors()); // Utilisation de CORS pour permettre les requêtes cross-origin c'est-à-dire que le frontend peut communiquer avec le backend. Par exemeple, si le frontend est sur http://localhost:3000 et le backend sur http://localhost:5000, CORS permet au frontend d'accéder aux ressources du backend.
 
+app.use("/api/auth", authRoutes); // Importation des routes d'authentification. Ainsi, toutes les requêtes vers /api/auth seront gérées par le routeur authRoutes. En gros toutes les requetes qui viennent dans notre API avec le préfixe /api/auth seront gérées par les routes définies dans le fichier auth.ts (AuthRoutes).
 app.use("/api/users", userRoutes); // Importation des routes des utilisateurs. Ainsi, toutes les requêtes vers /api/users seront gérées par le routeur userRoutes. En gros toutes les requetes qui viennent dans notre API avec le préfixe /api/users seront gérées par les routes définies dans le fichier users.ts (UserRoutes).
 
 app.listen(7000, () => {
