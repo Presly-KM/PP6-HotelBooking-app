3d4afe03f19c5faba6d0efdae835cc68ad3fea8d
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-08-15 13:45:41 +0200

    add file

---
+++ backend/src/routes/my-hotels.ts
@@ -0,0 +1,8 @@
+import express, {Request, Response} from "express"; // Importation d'Express pour créer le serveur
+
+const router = express.Router();
+
+// api/my-hotels
+router.post("/", async (req: Request, res: Response) => {
+    
+})
